e0c872be1d7cbe742413f23b39549f585bd25abf
Andreiionas <andreiionas009@gmail.com>
2025-06-15 13:35:19 +0100

    cw1

---
+++ app/src/main/java/com/example/culinarycompanion/AddEditRecipeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.culinarycompanion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddEditRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
---
+++ app/src/main/java/com/example/culinarycompanion/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.culinarycompanion
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.btn_add_recipe)
+        addButton.setOnClickListener {
+            val intent = Intent(this, AddEditRecipeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
